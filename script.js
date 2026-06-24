4557c4a4cd441007769c4c48c5cae0071346fd1e
natacharyannyoliveira-star <natacha.ryanny.oliveira@escola.pr.gov.br>
2026-06-24 10:54:10 -0300

    Add click event to display sustainability message

---
+++ script.js
@@ -0,0 +1,7 @@
+const botao = document.getElementById("btnMensagem");
+const mensagem = document.getElementById("mensagem");
+
+botao.addEventListener("click", () => {
+    mensagem.textContent =
+        "🌎 Um agro forte e sustentável garante alimento, desenvolvimento e preservação para as futuras gerações!";
+});
